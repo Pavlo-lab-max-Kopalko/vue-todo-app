1ece4770f92c663fbfa8937600943049951318d8
Pavlo <oksanakopalko@gmail.com>
2025-11-30 20:59:44 +0200

    add watch and onBeforeMount

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,167 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref, computed, onBeforeMount, watch } from 'vue';
+  import originalTodos from './data/todos';
+
+  // let initialTodos = [];
+
+  // try {
+  //   initialTodos = JSON.parse(localStorage.getItem('todos'));
+  // } catch (error) {}
+
+  // if (!Array.isArray(initialTodos)) {
+  //   initialTodos = [];
+  // }
+
+  const todos = ref([]);
+
+  onBeforeMount(() => {
+    try {
+      todos.value = JSON.parse(localStorage.getItem('todos'));
+    } catch (error) {}
+
+    if (!Array.isArray(todos.value)) {
+      todos.value = [];
+    }
+  });
+
+  watch(
+    todos,
+    newTodos => {
+      localStorage.setItem('todos', JSON.stringify(newTodos));
+    },
+    { deep: true },
+  );
+
+  const title = ref("");
+  const activeTodos = computed(() =>
+    todos.value.filter((todo) => !todo.completed)
+  );
+
+  const status = ref('all');
+
+  const visibleTodos = computed(() => {
+    if (status.value === 'active') {
+      return activeTodos.value;
+    }
+
+    if (status.value === 'completed') {
+      return todos.value.filter(todo => todo.completed);
+    }
+
+    return todos.value;
+  });
+
+  function addTodo() {
+    if (!title.value) {
+      errorMessage.value = "Title should not be empty";
+
+      return;
+    };
+
+    todos.value.push({
+      id: Date.now(),
+      title: title.value,
+      completed: false,
+    });
+
+    title.value = "";
+  }
+
+  const errorMessage = ref("");
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  
+  <form @submit.prevent="addTodo">
+    
+    <input
+      class="todoapp__new-todo"
+      placeholder="What needs to be done?"
+      @input="errorMessage = ''"
+      v-model="title"
+    />
+  </form>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div
+    class="todo"
+    v-for="(todo, i) of visibleTodos"
+    :key="todo.id"
+    :class="{ completed: todo.completed }"
+  >
+    <label class="todo__status-label">
+      <input
+        type="checkbox"
+        class="todo__status"
+        v-model="todo.completed"
+      />
+    </label>
+
+    <form v-if="false">
+      <input 
+        type="text" 
+        class="todo__title-field" 
+        placeholder="Empty todo will be deleted" 
+      />
+    </form>
+
+    <template v-else>
+      <span class="todo__title">{{ todo.title }}</span>
+      <button class="todo__remove" @click="todos.splice(i, 1)">×</button>
+    </template>
+
+    <div class="modal overlay" :class="{ 'is-active': false }">
+      <div class="modal-background has-background-white-ter"></div>
+      <div class="loader"></div>
     </div>
-  </header>
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <button
+    class="todoapp__clear-completed"
+    :disabled="todos.length === activeTodos.length"
+    @click="todos = activeTodos"
+  >
+    Clear completed
+  </button>
+
+  <nav class="filter">
+    <a
+      href="#/"
+      class="filter__link"
+      :class="{ selected: status === 'all' }"
+      @click="status = 'all'"
+    >
+      All
+    </a>
+    <a
+      href="#/active"
+      class="filter__link"
+      :class="{ selected: status === 'active' }"
+      @click="status = 'active'"
+    >
+      Active
+    </a>
+    <a
+      href="#/completed"
+      class="filter__link"
+      :class="{ selected: status === 'completed' }"
+      @click="status = 'completed'"
+    >
+      Completed
+    </a>
+  </nav>
+
+  <div
+    class="notification is-danger is-light has-text-weight-normal" 
+    v-if="errorMessage"
+  >
+    <button 
+      class="delete"
+      @click="errorMessage = ''"
+    ></button>
+    {{ errorMessage }}
+  </div>
+
 </template>
 
 <style scoped>
